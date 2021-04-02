af5e0df4d111d7dfd39cf9f5dea0a989017795f8
Eldridge <elmarshdi@gmail.com>
2021-04-02 21:27:57 +0700

    completing menu box template

--- js/general.js
+++ js/general.js
@@ -1,9 +1,29 @@
 
 // Toggle burger button in navbar
 
-const toggleButton = document.querySelector('.toggle-btn')
+const toggleButton = document.querySelector('.burger-btn')
 const navbarLinks = document.querySelector('.navbar-links')
+const buttonLine = document.querySelectorAll('.burger-btn-line')
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+    
+    if(navbarLinks.className === 'navbar-links'){
+        navbarLinks.classList.toggle('active')
+        buttonLine[0].style.transformOrigin = '0 0'
+        buttonLine[0].style.transform = 'rotate(45deg) translate(-1px, 0px)'
+        buttonLine[1].style.display = 'none'
+        buttonLine[2].style.transformOrigin = '0 100%'
+        buttonLine[2].style.transform = 'rotate(-45deg) translate(-1px, 0px)'
+    }
+    else {
+        navbarLinks.classList.toggle('active')
+        buttonLine[0].style.transformOrigin = '0 100%'
+        buttonLine[0].style.transform = 'rotate(0deg)'
+        buttonLine[1].style.display = 'flex'
+        buttonLine[2].style.transformOrigin = '0 0'
+        buttonLine[2].style.transform = 'rotate(0deg)'
+        
+    }    
 })
+
+// End of toggle burger button
