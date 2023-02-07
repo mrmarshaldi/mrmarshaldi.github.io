82f52365c6bf3583df027f48f054f66f40d54e06
mrmarshaldi <elmarshdi@gmail.com>
2023-02-07 22:23:29 +0700

    Add banner

--- js/lightbox.js
+++ js/lightbox.js
@@ -57,7 +57,7 @@ function showSlides() {
 
   slides[slideIndex - 1].style.display = "block";
 
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
 }
 
 // End of Image Carousel
